fc8ac64c0ef5429ded269b0e25ac8788a097640f
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-03-23 17:12:07 -0300

    constructors and this keyword

--- OopsIsFun/MainClass.java
+++ OopsIsFun/MainClass.java
@@ -2,6 +2,7 @@ package OopsIsFun;
 
 public class MainClass {
     public static void main(String[] args) {
+        
         Person p1 = new Person();
         p1.age = 20;
         p1.name = "Raman";
@@ -14,6 +15,9 @@ public class MainClass {
 
         System.out.println(p2.age + " " + p2.name);
 
+        Person p3 = new Person(22,"Amandeep");
+
+        System.out.println(p3.age + " " + p3.name);
         p1.eat();
         p2.walk();
         p2.walk(2);
@@ -24,12 +28,25 @@ public class MainClass {
 class Person{
     int age;
     String name;
+
+    public Person(){
+        System.out.println("Creating an object");
+    }
+
+    public Person(int age, String name){
+        this();
+        this.age = age;
+        this.name = name;
+    }
+
     void walk(){
         System.out.println(name + " is walking.");
     }
+
     void eat(){
         System.out.println(name + " is eating.");
     }
+
     void walk(int steps){
         System.out.println(name + " walked " + steps + " steps.");
     }
