6d16d0b4f8b9aceb110503794a09a409019d451b
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-03-23 17:34:57 -0300

    inheritance, super keyword, and compile time polymorphism

--- OopsIsFun/MainClass.java
+++ OopsIsFun/MainClass.java
@@ -2,39 +2,45 @@ package OopsIsFun;
 
 public class MainClass {
     public static void main(String[] args) {
-        
-        Person p1 = new Person();
-        p1.age = 20;
-        p1.name = "Raman";
 
-        System.out.println(p1.age + " " + p1.name);
+//        Person p1 = new Person();
+//        p1.age = 20;
+//        p1.name = "Raman";
+//
+//        System.out.println(p1.age + " " + p1.name);
+//
+//        Person p2 = new Person();
+//        p2.age = 21;
+//        p2.name = "Aman";
+//
+//        System.out.println(p2.age + " " + p2.name);
+//
+//        Person p3 = new Person(22,"Amandeep");
+//
+//        System.out.println(p3.age + " " + p3.name);
+//        p1.eat();
+//        p2.walk();
+//        p2.walk(2);
 
-        Person p2 = new Person();
-        p2.age = 21;
-        p2.name = "Aman";
+        Developer d1 = new Developer(24, "Raman");
+        d1.walk();
 
-        System.out.println(p2.age + " " + p2.name);
-
-        Person p3 = new Person(22,"Amandeep");
-
-        System.out.println(p3.age + " " + p3.name);
-        p1.eat();
-        p2.walk();
-        p2.walk(2);
-
-
+    }
+}
+class Developer extends Person{
+    public Developer(int age, String name){
+        super(age, name);
+    }
+    void walk(){
+        System.out.println("Developer " + name + " is walking.");
     }
 }
 class Person{
     int age;
     String name;
 
-    public Person(){
-        System.out.println("Creating an object");
-    }
 
     public Person(int age, String name){
-        this();
         this.age = age;
         this.name = name;
     }
@@ -49,5 +55,9 @@ class Person{
 
     void walk(int steps){
         System.out.println(name + " walked " + steps + " steps.");
+    }
+
+    void doWork(){
+        System.out.println("The person is working.");
     }
 }
