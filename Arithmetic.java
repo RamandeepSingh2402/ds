1ee6293f79ce05bd461e6784b88c91442b5dc783
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-03-29 19:03:19 -0300

    using the different classes to find the sum

---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int first = in.nextInt();
+        int second = in.nextInt();
+        Sum add = new Sum(first, second);
+
+        System.out.println("The sum of the two numbers entered by you is: " + add.returnSum());
+    }
+}
+class Sum{
+    int a;
+    int b;
+    int sum;
+    public Sum(int a, int b){
+        this.a = a;
+        this.b = b;
+        this.sum = a + b;
+    }
+    public int returnSum(){
+        return this.sum;
+    }
+}
