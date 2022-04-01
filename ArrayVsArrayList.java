96fa01f02cfeee7cc895b5677e1c354ec0177c51
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-01 14:39:15 -0300

    some more cleaning and extracting, printing the elements from array and arrayList

--- ArrayVsArrayList.java
+++ ArrayVsArrayList.java
@@ -6,7 +6,8 @@ public class ArrayVsArrayList {
     public static void main(String[] args) {
 
         //declaring and initializing the arrays with predefined size, and predefined values as well
-        String[] friendsNameArray2 = {"John", "Chris", "Eric", "Juliano"};
+        //indexes: [0][1][2][3]
+        String[] friendsNameArray = {"John", "Chris", "Eric", "Juliano"};
 
         /*declaring and initializing an arrayList with predefined values
         Arrays.asList() can be used alone as well, but it gives us immutable list (that we can't alter)
@@ -22,9 +23,12 @@ public class ArrayVsArrayList {
          * CAN ONLY HOLD OBJECTSS
          * BUT SUPPORTS WRAPPER TYPES FOR PRIMITIVES (USING INTEGER INSTEAD OF int)
          */
-        ArrayList<String> friendsNameArrayList2 =
-                new ArrayList<>(Arrays.asList("John", "Chris", "Eric", "Juliano"));
 
+        ArrayList<String> friendsNameArrayList = new ArrayList<>(Arrays.asList("John", "Chris", "Eric", "Juliano"));
 
+        //extracting elements from both, the array as well as the arrayList
+        System.out.println(friendsNameArray[1]);
+        System.out.println(friendsNameArrayList.get(1));
+        
     }
 }
