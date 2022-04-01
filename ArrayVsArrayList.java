b8be7e3a55cbcbac4b70d1836d86c796c458b968
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-01 14:18:57 -0300

    made, initialized the arrays and the arrayList

---
+++ ArrayVsArrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayVsArrayList {
+    public static void main(String[] args) {
+        String[] friendsName = new String[4];
+        String[] friendsName2 = {"John", "Chris", "Eric", "Juliano"};
+
+        ArrayList<String> friendsArrayList = new ArrayList<>();
+    }
+}
