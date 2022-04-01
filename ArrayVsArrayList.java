73012604b0dcf119826aeb700a4163dcd7733a2a
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-01 14:44:40 -0300

    adding elements at the end of the ArrayList

--- ArrayVsArrayList.java
+++ ArrayVsArrayList.java
@@ -29,6 +29,23 @@ public class ArrayVsArrayList {
         //extracting elements from both, the array as well as the arrayList
         System.out.println(friendsNameArray[1]);
         System.out.println(friendsNameArrayList.get(1));
-        
+
+        //printing the size of each
+
+        //field ".length" for getting the size of the array
+        System.out.println(friendsNameArray.length);
+        //method call ".size()" for getting the size of the arrayList
+        System.out.println(friendsNameArrayList.size());
+
+        /**
+         * How to add elements at the end of both??
+         * That can't be done for the arrays, as they have a fixed size!
+         * ".add()" method will do the work for you for the arrayList
+         */
+
+        //adding elements at the end of an arrayList
+        friendsNameArrayList.add("Mitch");
+        System.out.println(friendsNameArrayList.get(4));
+
     }
 }
