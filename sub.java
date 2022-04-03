f0aafa9b43151edb70f943e8314e1d34e3383fef
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-02 23:32:53 -0300

    commented the main method

--- sub.java
+++ sub.java
@@ -1,15 +1,31 @@
 import java.util.Scanner;
 
 public class sub {
+
+    //main method
     public static void main(String[] args) {
+        //importing the scanner class
         Scanner in = new Scanner(System.in);
+
+        //prompt for the user to enter the large number first
         System.out.print("Enter the larger number: ");
+
+        //storing the value entered by the user in the variable first
         int first = in.nextInt();
+
+        //prompt for the user to enter the smaller number
         System.out.print("\nEnter the smaller number: ");
+
+        //storing the value entered by the user in the varibable second
         int second = in.nextInt();
+
+        //creating an object of the type Subt
         Subt sub = new Subt(first, second);
+
+        //getting the result of the subtraction, and storing the same in the variable result
         int result = sub.getSubt();
 
+        //printing the result of the subtraction to the console
         System.out.println("The result of the subtraction is: " + result);
     }
 }
