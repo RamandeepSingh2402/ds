97f6a499c8dfa75da52017de3ce6b52775bcd5a3
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-02 23:28:59 -0300

    commented the Subt class

--- sub.java
+++ sub.java
@@ -13,13 +13,17 @@ public class sub {
         System.out.println("The result of the subtraction is: " + result);
     }
 }
+//Subt class
 class Subt{
-
+    //instance variable
     protected int subt;
+
+    //constructor for the Subt class
     public Subt(int first, int second){
      this.subt = first -second;
     }
 
+    //getter method for the result of the subtraction
     public int getSubt(){
         return this.subt;
     }
