5a1b10b9986f063559c7a7fe71956e383cf9f692
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-03 23:48:17 -0300

    added the getter method for the result, and printed the result to the console

--- Div.java
+++ Div.java
@@ -7,11 +7,17 @@ public class Div {
         int den = in.nextInt();
 
         DivResult div = new DivResult(num, den);
+
+        System.out.println("The result of the division is:" +div.getResult());
     }
 }
 class DivResult{
     private int result;
     public DivResult(int num, int den){
         this.result = num/den;
+    }
+
+    public int getResult() {
+        return result;
     }
 }
