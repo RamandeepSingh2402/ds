cd3d6c012233d9563f32e6deb6b6e86ccfeec5c1
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-03 23:44:04 -0300

    imported the scanner class and took the inputs for numerator and denominator

--- Div.java
+++ Div.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Div {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int den = in.nextInt();
     }
 }
