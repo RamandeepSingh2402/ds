4d59415dcd2e6333e4210e201fd86e11ce62c08d
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-03 23:53:42 -0300

    commented the main method

--- Div.java
+++ Div.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
 public class Div {
+    
+    //main method
     public static void main(String[] args) {
+
+        //importing the scanner class
         Scanner in = new Scanner(System.in);
+
+        //prompting the user to enter the integer numerator
         System.out.print("Kindly enter the integer numerator: ");
+
+        //taking in the input of the numerator
         int num = in.nextInt();
 
+        //prompting the user to enter the integer denominator
         System.out.print("\nKindly enter the integer denominator: ");
+
+        //taking in the input of the denominator
         int den = in.nextInt();
 
+        //creating an object div of the type DivResult
         DivResult div = new DivResult(num, den);
 
+        //printing the result of the division by invoking the getter method of DivResult
         System.out.println("\nThe result of the division is: " + div.getResult());
     }
 }
