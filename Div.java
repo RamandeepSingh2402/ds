64c177e0487dade41ab77945d9a7911a6cfdfc3c
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-03 23:46:36 -0300

    created a new DivResult class,which calculates the int quotient

--- Div.java
+++ Div.java
@@ -5,5 +5,13 @@ public class Div {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
         int den = in.nextInt();
+
+        DivResult div = new DivResult(num, den);
+    }
+}
+class DivResult{
+    private int result;
+    public DivResult(int num, int den){
+        this.result = num/den;
     }
 }
