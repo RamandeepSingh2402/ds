bf95bf85e4fefaa6250062eeb8346c09447b26bd
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-05 18:51:51 -0300

    created constructor of BT in BT.java

--- BT.java
+++ BT.java
@@ -6,4 +6,9 @@ public class BT<T> {
     private BT<T> parent;
     private BT<T> left;
     private BT<T> right;
+    public BT(){
+        parent = left= right = null;
+        data = null;
+    }
+    
 }
