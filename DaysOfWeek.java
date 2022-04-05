a9cee1e713eb66ce8e988075ef6a44b3a6961a7f
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-04 22:21:15 -0300

    created the strings for the days, and introduced for loop to print the days of the week

--- DaysOfWeek.java
+++ DaysOfWeek.java
@@ -1,5 +1,34 @@
 public class DaysOfWeek {
     public static void main(String[] args) {
-        
+        String mon = "Monday";
+        String tue = "Tuesday";
+        String wed = "Wednesday";
+        String thu = "Thursday";
+        String fri = "Friday";
+        String sat = "Saturday";
+        String sun = "Sunday";
+        for (int i = 1; i < 8; i++) {
+            if(i==1){
+                System.out.println(i + " " + mon);
+            }
+            if(i==2){
+                System.out.println(i + " " +tue);
+            }
+            if(i==3){
+                System.out.println(i + " " +wed);
+            }
+            if(i==4){
+                System.out.println(i + " " +thu);
+            }
+            if(i==5){
+                System.out.println(i + " " +fri);
+            }
+            if(i==6){
+                System.out.println(i + " " +sat);
+            }
+            if(i==7){
+                System.out.println(i + " " +sun);
+            }
+        }
     }
 }
