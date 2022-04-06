04174d3510cad82be7f9008da460c8e22543d9f0
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-05 21:24:42 -0300

    commented the BT method

--- BT.java
+++ BT.java
@@ -1,22 +1,30 @@
+//BT class
 public class BT<T> {
+
+    //instance variables for BT class
     private T data;
     private BT<T> parent;
     private BT<T> left;
     private BT<T> right;
+
+    //constructor for the BT class
     public BT(){
         parent = left= right = null;
         data = null;
     }
 
+    //isEmpty() method, which returns boolean value based on the data contained in the BT
     public boolean isEmpty(){
         return data == null;
     }
 
+    //clear method, used to clear the tree, garbage collector takes the inserted values
     public void clear(){
         left = right = parent = null;
         data = null;
     }
 
+    //makeRoot method, which makes the root of some given data
     public void makeRoot(T data){
         if(!isEmpty()){
             System.out.println("Can't make root, it already exists.");
@@ -25,38 +33,48 @@ public class BT<T> {
             this.data = data;
         }
     }
+
+    //setData() method, which is a setter method for data
     public void setData(T data){
         this.data = data;
     }
 
+    //setLeft() method, which is a setter method for setting the left BinaryTree
     public void setLeft(BT<T> tree){
         left = tree;
     }
 
+    //setRight() method, which is a setter method for setting the right BinaryTree
     public void setRight(BT<T> tree){
         right = tree;
     }
 
+    //setParent() method, which is a setter method for setting the parent of a BinaryTree
     public void setParent(BT<T> tree){
         parent = tree;
     }
 
+    //getData() method, which is a getter method for getting the data of a BinaryTree
     public T getData(){
         return data;
     }
 
+    //getParent() method, which is a getter method for getting the parent of a BinaryTree
     public BT<T> getParent(){
         return parent;
     }
 
+    //getLeft() method, which is a getter method for getting the left tree of a BinaryTree
     public BT<T> getLeft(){
         return left;
     }
 
+    //getRight() method, which is a getter method for getting the right tree of a BinaryTree
     public BT<T> getRight(){
         return right;
     }
 
+    //attachLeft() method, which attaches the tree to the left of BinaryTree
     public void attachLeft(BT<T> tree){
         if(tree == null){
             return;
@@ -71,6 +89,7 @@ public class BT<T> {
         }
     }
 
+    //attachRight() method, which attaches the tree to the right of BinaryTree
     public void attachRight(BT<T> tree){
         if(tree == null){
             return;
@@ -85,6 +104,7 @@ public class BT<T> {
         }
     }
 
+    //detachhLeft() method, which detaches the tree to the left of BinaryTree
     public BT<T> detachLeft(){
         if(this.isEmpty()){
             return null;
@@ -96,6 +116,7 @@ public class BT<T> {
         return retLeft;
     }
 
+    //detachRight() method, which detaches the tree to the right of BinaryTree
     public BT<T> detachRight(){
         if(this.isEmpty()) {
             return null;
@@ -107,6 +128,7 @@ public class BT<T> {
         return retRight;
     }
 
+    //root() method, which makes root of the tree
     public BT<T> root(){
         if(this.isEmpty()){
             return null;
@@ -121,6 +143,7 @@ public class BT<T> {
         return next;
     }
 
+    //preorder() method for preOrder traversal of a Binary Tree
     public static<T> void preOrder(BT<T> tree){
         if(tree!=null){
             System.out.println(tree.getData() + "\t");
@@ -129,6 +152,7 @@ public class BT<T> {
         }
     }
 
+    //inorder() method for inOrder traversal of a Binary Tree
     public static<T> void inOrder(BT<T> tree){
         if(tree!=null){
             inOrder(tree.getLeft());
@@ -137,6 +161,7 @@ public class BT<T> {
         }
     }
 
+    //postorder() method for postOrder traversal of a Binary Tree
     public static<T> void postOrder(BT<T> tree){
         if(tree!=null){
             postOrder(tree.getLeft());
