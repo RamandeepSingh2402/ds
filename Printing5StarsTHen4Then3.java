c5da75e1179a2809e68feb49e17cdf3e962a3d75
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-14 14:29:07 -0300

    completed the switch statements to print the stars to the console

--- Printing5StarsTHen4Then3.java
+++ Printing5StarsTHen4Then3.java
@@ -1,6 +1,6 @@
 public class Printing5StarsTHen4Then3 {
     public static void main(String[] args) {
-        for (int i = 5; i >0 ; i++) {
+        for (int i = 5; i >0 ; i--) {
             switch (i){
                 case 5:
                     System.out.println("*****");
@@ -10,7 +10,17 @@ public class Printing5StarsTHen4Then3 {
                     System.out.println("****");
                     break;
 
-                    
+                case 3:
+                    System.out.println("***");
+                    break;
+
+                case 2:
+                    System.out.println("**");
+                    break;
+
+                case 1:
+                    System.out.println("*");
+                    break;
             }
         }
     }
