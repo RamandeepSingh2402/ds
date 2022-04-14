12ffc14e358ce151d8303ad2be8fea9eb301b69b
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-14 14:25:43 -0300

    created an if conditional followed by print statements

--- Printing5StarsTHen4Then3.java
+++ Printing5StarsTHen4Then3.java
@@ -1,7 +1,10 @@
 public class Printing5StarsTHen4Then3 {
     public static void main(String[] args) {
         for (int i = 5; i >0 ; i++) {
-
+            if(i==5){
+                System.out.println("*****");
+            }
+            
         }
     }
 }
