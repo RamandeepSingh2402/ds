11631133dd652a84ab4edd6eb6301ade69ae50ae
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-14 14:27:21 -0300

    used a switch instead of using if conditions

--- Printing5StarsTHen4Then3.java
+++ Printing5StarsTHen4Then3.java
@@ -1,10 +1,17 @@
 public class Printing5StarsTHen4Then3 {
     public static void main(String[] args) {
         for (int i = 5; i >0 ; i++) {
-            if(i==5){
-                System.out.println("*****");
+            switch (i){
+                case 5:
+                    System.out.println("*****");
+                    break;
+
+                case 4:
+                    System.out.println("****");
+                    break;
+
+                    
             }
-            
         }
     }
 }
