c9aef789b86dbc0bf4be47e598b1115b927c690f
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-14 14:24:35 -0300

    changed the logic of the for loop

--- Printing5StarsTHen4Then3.java
+++ Printing5StarsTHen4Then3.java
@@ -1,7 +1,7 @@
 public class Printing5StarsTHen4Then3 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
-            
+        for (int i = 5; i >0 ; i++) {
+
         }
     }
 }
