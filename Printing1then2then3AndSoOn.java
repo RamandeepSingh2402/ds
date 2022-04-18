0c40376149373a55cf03c080e9df0b6d886a9ca1
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-17 22:18:02 -0300

    created switch for printing the stars

--- Printing1then2then3AndSoOn.java
+++ Printing1then2then3AndSoOn.java
@@ -1,7 +1,10 @@
 public class Printing1then2then3AndSoOn {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            
+            switch (i){
+                case 1:
+                    System.out.println("*");
+            }
         }
     }
 }
