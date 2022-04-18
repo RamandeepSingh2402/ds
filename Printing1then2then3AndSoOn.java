908f5c9f79a84665059e15be454ce70a334f5f1c
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-04-17 22:19:57 -0300

    completed the switch statement for printing the stars

--- Printing1then2then3AndSoOn.java
+++ Printing1then2then3AndSoOn.java
@@ -1,9 +1,26 @@
 public class Printing1then2then3AndSoOn {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i <= 5; i++) {
             switch (i){
                 case 1:
                     System.out.println("*");
+                    break;
+
+                case 2:
+                    System.out.println("**");
+                    break;
+
+                case 3:
+                    System.out.println("***");
+                    break;
+
+                case 4:
+                    System.out.println("****");
+                    break;
+
+                case 5:
+                    System.out.println("*****");
+                    break;
             }
         }
     }
